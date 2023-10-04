f6dfc94506df83e7e9952d314f8e1cad2f1689e3
franveggiani <francoveggiani@gmail.com>
2023-10-04 19:21:29 -0300

    productoEstadoProducto, rubroArticuloIngrediente

--- src/main/java/com/tup/buensabor/BuensaborApplication.java
+++ src/main/java/com/tup/buensabor/BuensaborApplication.java
@@ -1,13 +1,13 @@
 package com.tup.buensabor;
 
-import com.tup.buensabor.entities.Domicilio;
-import com.tup.buensabor.entities.Pedido;
-import com.tup.buensabor.entities.Persona;
-import com.tup.buensabor.entities.Usuario;
+import com.tup.buensabor.entities.*;
+import com.tup.buensabor.enums.EstadoPedido;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
 
 @SpringBootApplication
 public class BuensaborApplication {
@@ -22,6 +22,7 @@ public class BuensaborApplication {
 		return args -> {
 			System.out.println("------------------------Estoy Funcionando------------------------");
 
+			//Domicilio
 
 			Domicilio domicilio=new Domicilio();
 			domicilio.setCalle("Olascoaga");
@@ -30,22 +31,28 @@ public class BuensaborApplication {
 			domicilio.setNumeroDpto(23);
 			domicilio.setPisoDpto(2);
 
+			//Persona
+
 			Persona persona=new Persona();
 			persona.setNombre("Agustin");
 			persona.setApellido("Sulan");
 			persona.setEmail("agustinsula@gmail.com");
 			persona.setTelefono("2627867986");
 
+			//User
+
 			Usuario user=new Usuario();
 			user.setUsername("AguSul12");
 			user.setAuth0Id("1f89JF81FSA2415G5UUES");
 
+			//Pedido
 
 			Pedido pedido1=new Pedido();
 			pedido1.setEstado("En Preparacion");
 			pedido1.setTipoEnvio("Aero");
 			pedido1.setTotal(23322);
 
+			//Producto
 
 			Producto producto1=new Producto();
 			producto1.setTiempoEstimadoCocina(30);
@@ -58,6 +65,7 @@ public class BuensaborApplication {
 			producto1.setStockMinimo(100);
 			producto1.setUnidadMedida("ctdad");
 
+			//Factura
 
 			Factura factura1=new Factura();
 			factura1.setTotal(2323);
@@ -65,16 +73,35 @@ public class BuensaborApplication {
 			factura1.setNumero(2735);
 			factura1.setFormaPago("Mercado Pago");
 
+			//DetallePedido
 
 			DetallePedido detallePedido = new DetallePedido();
 			detallePedido.setCantidad(3);
 			detallePedido.setSubTotal(80);
 
+			//PedidoEstadoPedido
 
+			PedidoEstadoPedido pedidoEstadoPedido1 = new PedidoEstadoPedido();
+			pedidoEstadoPedido1.setEstadoPedido(EstadoPedido.COMPLETADO);
+			pedidoEstadoPedido1.setFechaHoraCambioEstado(new Date());
 
-			Rubro rubro = new Rubro();
-			rubro.setDenominacion("Carnes");
+			PedidoEstadoPedido pedidoEstadoPedido2 = new PedidoEstadoPedido();
+			pedidoEstadoPedido2.setEstadoPedido(EstadoPedido.COMPLETADO);
+			pedidoEstadoPedido2.setFechaHoraCambioEstado(new Date());
 
+			//RubroIngrediente
+
+			RubroArticuloIngrediente rubroArticuloIngrediente1 = new RubroArticuloIngrediente();
+			rubroArticuloIngrediente1.setDenominacion("verdura");
+			rubroArticuloIngrediente1.setFechaAlta(new Date());
+			rubroArticuloIngrediente1.setFehaBaja(new Date());
+			rubroArticuloIngrediente1.setFechaModificacion(new Date());
+
+			RubroArticuloIngrediente rubroArticuloIngrediente2 = new RubroArticuloIngrediente();
+			rubroArticuloIngrediente1.setDenominacion("condimento");
+			rubroArticuloIngrediente1.setFechaAlta(new Date());
+			rubroArticuloIngrediente1.setFehaBaja(new Date());
+			rubroArticuloIngrediente1.setFechaModificacion(new Date());
 
 
 			//Asociaciones
