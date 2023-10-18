a33b148397b9956e8ecb31a06d96f288be1eabf7
franveggiani <francoveggiani@gmail.com>
2023-10-18 16:49:53 -0300

    Implementación de registro de usuario: Query para validar que usuario ya existe

--- src/main/java/com/tup/buensabor/controllers/UsuarioController.java
+++ src/main/java/com/tup/buensabor/controllers/UsuarioController.java
@@ -4,16 +4,14 @@ import com.tup.buensabor.entities.Usuario;
 import com.tup.buensabor.services.UsuarioServiceImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin(origins = "*")
 @RequestMapping(path = "api/v1/usuario")
 public class UsuarioController extends BaseControllerImpl<Usuario, UsuarioServiceImpl>{
 
+    @GetMapping("/registrar")
     public ResponseEntity<?> registrarUsuario(@RequestBody Usuario usuario) {
         try {
             return ResponseEntity.status(HttpStatus.OK).body(service.registrarUsuario(usuario));
