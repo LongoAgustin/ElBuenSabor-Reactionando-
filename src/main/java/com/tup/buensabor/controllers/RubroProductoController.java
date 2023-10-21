620bcc8c136047f76f023fe49236358e41b7a189
LeitoGonzalez <141784522+LeitoGonzalez@users.noreply.github.com>
2023-10-20 21:54:50 -0300

    abm RubroProducto

--- src/main/java/com/tup/buensabor/controllers/RubroProductoController.java
+++ src/main/java/com/tup/buensabor/controllers/RubroProductoController.java
@@ -1,13 +1,33 @@
 package com.tup.buensabor.controllers;
 
+import com.tup.buensabor.entities.RubroIngrediente;
 import com.tup.buensabor.entities.RubroProducto;
 import com.tup.buensabor.services.RubroProductoServiceImpl;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin(origins = "*")
 @RequestMapping(path = "api/v1/rubroProducto")
 public class RubroProductoController extends BaseControllerImpl<RubroProducto, RubroProductoServiceImpl>{
+
+    @PostMapping("/newRubroProducto")
+    public ResponseEntity<?> newRubroProducto(@RequestBody RubroProducto rubroProducto) {
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(service.newRubroProducto(rubroProducto));
+        } catch (Exception e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
+
+
+    @PostMapping("/updateRubroProducto/{id}")
+    public ResponseEntity<?> updateRubroProducto(@PathVariable Long id, @RequestBody RubroProducto rubroProducto) {
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(service.updateRubroProducto(id, rubroProducto));
+        } catch (Exception e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
 }
