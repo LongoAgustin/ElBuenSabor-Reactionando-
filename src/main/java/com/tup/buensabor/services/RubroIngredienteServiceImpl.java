dfb89fd07c4e0dc41c2a5b5595aca96768433634
LeitoGonzalez <141784522+LeitoGonzalez@users.noreply.github.com>
2023-10-20 21:42:08 -0300

    abm RubroIngrediente

--- src/main/java/com/tup/buensabor/services/RubroIngredienteServiceImpl.java
+++ src/main/java/com/tup/buensabor/services/RubroIngredienteServiceImpl.java
@@ -6,6 +6,10 @@ import com.tup.buensabor.repositories.RubroIngredienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class RubroIngredienteServiceImpl extends BaseServiceImpl<RubroIngrediente, Long> implements RubroIngredienteService {
 
@@ -15,5 +19,49 @@ public class RubroIngredienteServiceImpl extends BaseServiceImpl<RubroIngredient
     public RubroIngredienteServiceImpl(BaseRepository<RubroIngrediente,Long> baseRepository, RubroIngredienteRepository rubroIngredienteRepository) {
         super(baseRepository);
         this.rubroIngredienteRepository = rubroIngredienteRepository;
+    }
+
+    @Override
+    public RubroIngrediente newRubroIngrediente(RubroIngrediente rubroIngrediente) throws Exception {
+        try{
+            List<RubroIngrediente> rubrosIngredientesEncontrados = rubroIngredienteRepository.verificarRubroIngrediente(rubroIngrediente.getDenominacion());
+            if (rubrosIngredientesEncontrados.isEmpty()){
+                rubroIngredienteRepository.save(rubroIngrediente);
+                return rubroIngrediente;
+            }else {
+                throw new Exception("Este rubro ya existe");
+            }
+        }catch (Exception e){
+            throw new Exception(e.getMessage());
+        }
+    }
+
+
+
+    @Override
+    public RubroIngrediente updateRubroIngrediente(Long id, RubroIngrediente nuevosDatos) throws Exception {
+        try{
+            Optional<RubroIngrediente> rubroIngredienteOptional = rubroIngredienteRepository.findById(id);
+
+            if(rubroIngredienteOptional.isPresent()){
+                RubroIngrediente rubroIngrediente = rubroIngredienteOptional.get();
+
+                // Verifica y actualiza campos opcionales
+                if (nuevosDatos.getDenominacion() != null) {
+                    rubroIngrediente.setDenominacion(nuevosDatos.getDenominacion());
+                    rubroIngrediente.setFechaHoraModificacion(new Date());
+                }
+
+
+                // Guarda los cambios en la base de datos
+                rubroIngredienteRepository.save(rubroIngrediente);
+
+                return rubroIngrediente;
+            }else {
+                throw new Exception("Ese ingrediente no esta disponible");
+            }
+        }catch (Exception e){
+            throw new Exception(e.getMessage());
+        }
     }
 }
