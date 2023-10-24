9588af8322d1969cddd113f05d5a880f7a760df0
MaaR120 <martineguren120@gmail.com>
2023-10-23 21:05:40 -0300

    busqueda de pedidos

--- src/main/java/com/tup/buensabor/services/PedidoServiceImpl.java
+++ src/main/java/com/tup/buensabor/services/PedidoServiceImpl.java
@@ -1,10 +1,15 @@
 package com.tup.buensabor.services;
 
 import com.tup.buensabor.entities.Pedido;
+import com.tup.buensabor.entities.Usuario;
 import com.tup.buensabor.repositories.BaseRepository;
 import com.tup.buensabor.repositories.PedidoRepository;
+import com.tup.buensabor.repositories.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PedidoServiceImpl extends BaseServiceImpl<Pedido, Long> implements PedidoService{
@@ -12,8 +17,26 @@ public class PedidoServiceImpl extends BaseServiceImpl<Pedido, Long> implements 
     @Autowired
     protected PedidoRepository pedidoRepository;
 
+    @Autowired
+    protected UsuarioRepository usuarioRepository;
+
     public PedidoServiceImpl(BaseRepository<Pedido,Long> baseRepository, PedidoRepository pedidoRepository) {
         super(baseRepository);
         this.pedidoRepository = pedidoRepository;
     }
+
+    public List<Pedido> searchXUsuario(Long usuarioid) throws Exception{
+        try{
+            Optional<Usuario> user=usuarioRepository.findById(usuarioid);
+            if(user.isEmpty()){
+                throw new Exception("No Existe el Usuario");
+            }else {
+                List<Pedido> pedidos=user.get().getPedidoList();
+                return pedidos;
+            }
+        }catch (Exception e){
+            throw new Exception(e.getMessage());
+        }
+    }
+
 }
