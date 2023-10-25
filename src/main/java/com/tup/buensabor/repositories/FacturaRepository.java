88e1bfd68c63a30dd8b617e8697aee2fb897a689
Juan Caballero <jcaballero662@gmail.com>
2023-10-25 19:28:02 -0300

    historia usuario 18 v2

--- src/main/java/com/tup/buensabor/repositories/FacturaRepository.java
+++ src/main/java/com/tup/buensabor/repositories/FacturaRepository.java
@@ -8,6 +8,9 @@ import java.util.List;
 
 @Repository
 public interface FacturaRepository extends BaseRepository<Factura, Long> {
+
+    //En esta consulta, se seleccionan los ID del pedido, el cliente y la factura de las tablas correspondientes (reemplaza Pedido, Cliente, y Factura con los nombres reales de tus entidades).
+    // La consulta realiza una unión (JOIN) entre las tablas Pedido y Cliente, y una unión izquierda (LEFT JOIN) con la tabla Factura. Luego, se aplican condiciones para que el cliente se llame "Agustin", el estado del pedido sea "pagado" y no haya una factura asociada al pedido.
     @Query(value = "SELECT p.id as pedido_id, c.id as cliente_id, f.id as factura_id " +
             "FROM Pedido p " +
             "JOIN Cliente c ON p.cliente_id = c.id " +
