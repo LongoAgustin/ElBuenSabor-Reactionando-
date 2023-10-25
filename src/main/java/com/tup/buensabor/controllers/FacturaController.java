edbdc3408bdb22ec52303747d7eacce1b1554db7
Juan Caballero <jcaballero662@gmail.com>
2023-10-25 19:21:25 -0300

    historia usuario 18

--- src/main/java/com/tup/buensabor/controllers/FacturaController.java
+++ src/main/java/com/tup/buensabor/controllers/FacturaController.java
@@ -3,6 +3,7 @@ package com.tup.buensabor.controllers;
 import com.tup.buensabor.entities.Factura;
 import com.tup.buensabor.services.FacturaServiceImpl;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "*")
 @RequestMapping(path = "api/v1/factura")
 public class FacturaController extends BaseControllerImpl<Factura, FacturaServiceImpl>{
+    @GetMapping("search")
+    void emitirFacturasParaClienteAgustin() {
+
+    }
+
+
 }
--- src/main/java/com/tup/buensabor/repositories/FacturaRepository.java
+++ src/main/java/com/tup/buensabor/repositories/FacturaRepository.java
@@ -1,8 +1,20 @@
 package com.tup.buensabor.repositories;
 
 import com.tup.buensabor.entities.Factura;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface FacturaRepository extends BaseRepository<Factura, Long> {
+    @Query(value = "SELECT p.id as pedido_id, c.id as cliente_id, f.id as factura_id " +
+            "FROM Pedido p " +
+            "JOIN Cliente c ON p.cliente_id = c.id " +
+            "LEFT JOIN Factura f ON p.id = f.pedido_id " +
+            "WHERE c.nombre = 'Agustin' AND p.estado = 'COBRADO' AND f.id IS NULL", nativeQuery = true)
+    List<Factura[]> obtenerPedidoParaFacturar();
+
+
+
 }
