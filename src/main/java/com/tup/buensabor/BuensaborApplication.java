6f41b0029c53bd15fcc0eb9981433648cb3c79ce
Alexandra Fernandez <fernandezmariaalexandra27@gmail.com>
2023-11-14 18:55:39 -0300

    Unidad y Rubro

--- src/main/java/com/tup/buensabor/BuensaborApplication.java
+++ src/main/java/com/tup/buensabor/BuensaborApplication.java
@@ -93,13 +93,13 @@ public class BuensaborApplication {
 			//RubroIngrediente
 
 			RubroIngrediente rubroIngrediente1 = new RubroIngrediente();
-			rubroIngrediente1.setDenominacion("verdura");
+			rubroIngrediente1.setDenominacion("Verdura");
 			rubroIngrediente1.setFechaHoraAlta(new Date());
 			rubroIngrediente1.setFechaHoraBaja(new Date());
 			rubroIngrediente1.setFechaHoraModificacion(new Date());
 
 			RubroIngrediente rubroIngrediente2 = new RubroIngrediente();
-			rubroIngrediente2.setDenominacion("condimento");
+			rubroIngrediente2.setDenominacion("Condimento");
 			rubroIngrediente2.setFechaHoraAlta(new Date());
 			rubroIngrediente2.setFechaHoraBaja(new Date());
 			rubroIngrediente2.setFechaHoraModificacion(new Date());
@@ -124,9 +124,9 @@ public class BuensaborApplication {
 
 			UnidadMedida unidadMedida2 = new UnidadMedida();
 
-			unidadMedida.setDenominacion("unidad");
-			unidadMedida.setAbreviatura("unid");
-			unidadMedida.setFechaHoraAlta(new Date());
+			unidadMedida2.setDenominacion("unidad");
+			unidadMedida2.setAbreviatura("uni");
+			unidadMedida2.setFechaHoraAlta(new Date());
 
 			//Ingrediente
 
@@ -134,39 +134,39 @@ public class BuensaborApplication {
 
 			ingrediente.setDenominacion("Orégano");
 			ingrediente.setFechaHoraAlta(new Date());
-			ingrediente.setFechaHoraBaja(new Date());
-			ingrediente.setFechaHoraModificacion(new Date());
+			ingrediente.setFechaHoraBaja(null);
+			ingrediente.setFechaHoraModificacion(null);
 			ingrediente.setPrecioCompra(new BigDecimal(13215));
 			ingrediente.setStockMinimo(new BigDecimal(2165));
 			ingrediente.setStockActual(new BigDecimal(5900));
 			ingrediente.setUnidadMedida(unidadMedida);
-			ingrediente.setUrlImagen("");
+			ingrediente.setUrlImagen("https://plazavea.vteximg.com.br/arquivos/ids/419765-1000-1000/20146602.jpg?v=637375895601400000");
 			ingrediente.setRubroIngrediente(rubroIngrediente2);
 
 			Ingrediente ingrediente1 = new Ingrediente();
 
 			ingrediente1.setDenominacion("Perejil");
 			ingrediente1.setFechaHoraAlta(new Date());
-			ingrediente1.setFechaHoraBaja(new Date());
-			ingrediente1.setFechaHoraModificacion(new Date());
+			ingrediente1.setFechaHoraBaja(null);
+			ingrediente1.setFechaHoraModificacion(null);
 			ingrediente1.setPrecioCompra(new BigDecimal(1321));
 			ingrediente1.setStockMinimo(new BigDecimal(2195));
 			ingrediente1.setStockActual(new BigDecimal(590));
 			ingrediente1.setUnidadMedida(unidadMedida);
-			ingrediente1.setUrlImagen("");
+			ingrediente1.setUrlImagen("https://a1.soysuper.com/ecd11b05499d59551b6cfd055513fc54.1500.0.0.0.wmark.eacfaba2.jpg");
 			ingrediente1.setRubroIngrediente(rubroIngrediente2);
 
 			Ingrediente ingrediente2 = new Ingrediente();
 
 			ingrediente2.setDenominacion("Cebolla");
 			ingrediente2.setFechaHoraAlta(new Date());
-			ingrediente2.setFechaHoraBaja(new Date());
-			ingrediente2.setFechaHoraModificacion(new Date());
+			ingrediente2.setFechaHoraBaja(null);
+			ingrediente2.setFechaHoraModificacion(null);
 			ingrediente2.setPrecioCompra(new BigDecimal(13215));
 			ingrediente2.setStockMinimo(new BigDecimal(2165));
 			ingrediente2.setStockActual(new BigDecimal(5900));
 			ingrediente2.setUnidadMedida(unidadMedida2);
-			ingrediente2.setUrlImagen("");
+			ingrediente2.setUrlImagen("https://frutasyverduras.info/wp-content/uploads/2019/06/cebolla-morada-1024x711.jpg");
 			ingrediente2.setRubroIngrediente(rubroIngrediente1);
 
 			//DetalleProductoCocina
@@ -318,9 +318,12 @@ public class BuensaborApplication {
 			usuarioRepository.save(usuario);
 			domicilioRepository.save(domicilio);
 			unidadMedidaRepository.save(unidadMedida);
+			unidadMedidaRepository.save(unidadMedida2);
 			rubroIngredienteRepository.save(rubroIngrediente1);
 			rubroIngredienteRepository.save(rubroIngrediente2);
 			ingredienteRepository.save(ingrediente);
+			ingredienteRepository.save(ingrediente1);
+			ingredienteRepository.save(ingrediente2);
 			rubroProductoRepository.save(rubroProducto1);
 			productoRepository.save(articulo1);
 			pedidoRepository.save(pedido1);
